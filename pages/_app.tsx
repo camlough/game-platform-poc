c0e7c93802d6b93aad5b515fa35c3233ca5bdf5d
Cameron  Loughman <cameronloughman@gmail.com>
2022-12-28 10:45:41 -0800

    minor cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,37 +8,9 @@ import { useState } from 'react'
 
 import UserLayout from '../components/UserLayout'
 
-import { ReactNode } from "react";
-import { Layout } from '../components/UserLayout';
-// ** MUI Imports
-import { styled } from "@mui/material/styles";
-import Box, { BoxProps } from "@mui/material/Box";
-
 type ExtendedAppProps = AppProps & {
   Component: NextPage
 }
-
-// Styled component for Blank Layout component
-// const BlankLayoutWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-//   height: "100vh",
-
-//   // For V1 Blank layout pages
-//   "& .content-center": {
-//     display: "flex",
-//     minHeight: "100vh",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     padding: theme.spacing(5),
-//   },
-
-//   // For V2 Blank layout pages
-//   "& .content-right": {
-//     display: "flex",
-//     minHeight: "100vh",
-//     overflowX: "hidden",
-//     position: "relative",
-//   },
-// }));
 
 export default function App({ Component, pageProps }: ExtendedAppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
@@ -48,22 +20,7 @@ export default function App({ Component, pageProps }: ExtendedAppProps) {
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-
       {getLayout( <Component {...pageProps} />)}
     </SessionContextProvider>
   )
-
-  // const session = useSession();
-  // return (
-  //   <Layout session={session}>
-  //     <BlankLayoutWrapper className="layout-wrapper">
-  //       <Box
-  //         className="app-content"
-  //         sx={{ minHeight: "100vh", overflowX: "hidden", position: "relative" }}
-  //       >
-  //         <Component {...pageProps} />
-  //       </Box>
-  //     </BlankLayoutWrapper>
-  //   </Layout>
-  // );
 }
