d6d7554f869eac7866bf13346d8694118ea5d7dc
Cameron  Loughman <cameronloughman@gmail.com>
2022-12-29 13:25:44 -0800

    added pages for tictactoe and chess

---
+++ pages/play-game/chess/[id].tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const ChessGame = () => {
+    return (
+        <Typography variant="h2" sx={{textAlign: 'center'}}>Chess Coming Soon</Typography>
+    )
+}
+
+export default ChessGame;
